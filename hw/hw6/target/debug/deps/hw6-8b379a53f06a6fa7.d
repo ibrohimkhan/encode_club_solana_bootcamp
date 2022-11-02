63c083daeda370f9149fb5bbec4ebb865615590b
Ibrohim Kholmatov <ibrohimkhan@gmail.com>
2022-11-02 19:42:05 +0500

    hw6 task3

--- hw/hw6/target/debug/deps/hw6-8b379a53f06a6fa7.d
+++
@@ -1,5 +0,0 @@
-/Users/ibrahim/Code/solana/encode_club_solana_bootcamp/hw/hw6/target/debug/deps/hw6-8b379a53f06a6fa7: src/main.rs
-
-/Users/ibrahim/Code/solana/encode_club_solana_bootcamp/hw/hw6/target/debug/deps/hw6-8b379a53f06a6fa7.d: src/main.rs
-
-src/main.rs:
--- hw/hw6/target/debug/hw6.d
+++
@@ -1,1 +0,0 @@
-/Users/ibrahim/Code/solana/encode_club_solana_bootcamp/hw/hw6/target/debug/hw6: /Users/ibrahim/Code/solana/encode_club_solana_bootcamp/hw/hw6/src/main.rs
